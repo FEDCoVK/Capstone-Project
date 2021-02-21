e365e90e00f3f0b59c012f4868ba4fe20b3c5b05
Mubeenaslam69 <75478636+Mubeenaslam69@users.noreply.github.com>
2021-02-21 14:14:56 +0500

    Putting Alphabet Into Rectangles

--- KeyBoard/KeyAlphabet.py
+++ KeyBoard/KeyAlphabet.py
@@ -8,6 +8,21 @@ def blink(keyboard,choice_x,choice_y,w,h):
     cv2.rectangle(keyboard, (choice_x, choice_y), (choice_x + w, choice_y + h), (100, 100, 100), -1)
 
 def KeyAlphabet(x_axis,y_axis,x_blink,y_blink):
+    Alphabets = {0: "Q", 1: "W", 2: "E", 3: "R", 4: "T",
+                 5: "Y", 6: "U", 7: "I", 8: "0", 9: "P",
+                 10: "1", 11: "2", 12: "3", 13: "A", 14: "S", 15: "D",
+                 16: "F", 17: "G", 18: "H", 19: "J", 20: "K",
+                 21: "Enter", 22: "4", 23: "5", 24: "6", 25: "L",
+                 26: "Z", 27: "X", 28: "C", 29: "V", 30: "B", 31: "N", 32: "M", 33: "Up", 34: "7", 35: "8", 36: "10",
+                 37: "SpaceBar", 38: "BackSpace", 39: "Left", 40: "Down", 41: "Right", 42: "0"} # Taking Keys of Keyboard In array
+
+    width = 80
+    height = 80
+    font_letter = cv2.FONT_HERSHEY_PLAIN
+    font_size = 1.5
+    font_bold = 2
+
+
     x = 0
     y = 0
     keyboard = np.zeros((320, 900, 3), np.uint8)
@@ -19,9 +34,16 @@ def KeyAlphabet(x_axis,y_axis,x_blink,y_blink):
         y = y + 10
         x = x + 30
         for j in range(0, 13):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w, h)
-        blink(keyboard, choice_x=x_axis, choice_y=y_axis,w=x_blink,h=y_blink) 
-        x = x + 65
+            blink(keyboard, choice_x=x_axis, choice_y=y_axis,w=x_blink,h=y_blink) 
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
+            x = x + 65
             Counter = Counter + 1
         x = 0
         y = y + 75
@@ -30,15 +52,36 @@ def KeyAlphabet(x_axis,y_axis,x_blink,y_blink):
     for i in range(0, 1):
         x = x + 30
         for j in range(0, 8):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w, h)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
             x = x + 65
             Counter = Counter + 1
         for k in range(0, 1):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w * 2, h)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x + 28, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
             x = x + 130
             Counter = Counter + 1
         for l in range(0, 3):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w, h)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
             x = x + 65
             Counter = Counter + 1
         x = 0
@@ -48,16 +91,37 @@ def KeyAlphabet(x_axis,y_axis,x_blink,y_blink):
     for i in range(0, 1):
         x = x + 30
         for j in range(0, 8):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w, h)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69), font_bold)
+
             x = x + 65
             Counter = Counter + 1
         x = x + 35
         for k in range(0, 1):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             key(keyboard, x, y, w + 10, h)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
             x = x + 95
             Counter = Counter + 1
         for l in range(0, 3):
+            text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+            width_text, height_text = text_size[0], text_size[1]
+            text_x = int((width - width_text) / 2) + x
+            text_y = int((height + height_text) / 2) + y
+
             cv2.rectangle(keyboard, (x, y), (x + 60, y + h), (82, 69, 69), 1)
+            cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
             x = x + 65
             Counter = Counter + 1
         x = 0
@@ -68,21 +132,49 @@ def KeyAlphabet(x_axis,y_axis,x_blink,y_blink):
     for i in range(0, 1):
             x = x + 30
             for j in range(0, 1):
+                text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+                width_text, height_text = text_size[0], text_size[1]
+                text_x = int((width - width_text) / 2) + x
+                text_y = int((height + height_text) / 2) + y
+
                 key(keyboard, x, y, w, h)
+                cv2.putText(keyboard, Alphabets[Counter], (text_x + 100, text_y), font_letter, font_size, (82, 69, 69), font_bold)
+
                 x = x + 285
                 Counter = Counter + 1
             for k in range(0, 1):
+                text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+                width_text, height_text = text_size[0], text_size[1]
+                text_x = int((width - width_text) / 2) + x
+                text_y = int((height + height_text) / 2) + y
+
                 cv2.rectangle(keyboard, (x, y), (x + w - 105, y + h), (82, 69, 69), 1)
+                cv2.putText(keyboard, Alphabets[Counter], (text_x + 50, text_y), font_letter, font_size, (82, 69, 6),font_bold)
+
                 x = x + 175
                 Counter = Counter + 1
             x = x + 20
             for l in range(0, 3):
+                text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+                width_text, height_text = text_size[0], text_size[1]
+                text_x = int((width - width_text) / 2) + x
+                text_y = int((height + height_text) / 2) + y
+
                 cv2.rectangle(keyboard, (x, y), (x + 70, y + h), (82, 69, 69), 1)
+                cv2.putText(keyboard, Alphabets[Counter], (text_x - 5, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
                 x = x + 75
                 Counter = Counter + 1
             x = x + 10
             for m in range(0, 1):
+                text_size = cv2.getTextSize(Alphabets[Counter], font_letter, font_size, font_bold)[0]
+                width_text, height_text = text_size[0], text_size[1]
+                text_x = int((width - width_text) / 2) + x
+                text_y = int((height + height_text) / 2) + y
+
                 cv2.rectangle(keyboard, (x, y), (x + 60, y + h), (82, 69, 69), 1)
+                cv2.putText(keyboard, Alphabets[Counter], (text_x - 8, text_y), font_letter, font_size, (82, 69, 69),font_bold)
+
                 x = x + 70
                 Counter = Counter + 1
             x = 0
